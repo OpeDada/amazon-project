1d3f2b8cfbae4d9c10862e1c3bfc4f6468531d09
OpeDada <opedada@gmail.com>
2022-01-31 21:59:37 +0900

    import header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
 import SearchIcon from "@material-ui/icons/Search";
+import "../assets/stylesheets/Header.css";
+
 function Header() {
   return (
     <nav className="header">
