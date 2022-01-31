3b105bb8abd751bb61c28a68710813f704161b79
OpeDada <opedada@gmail.com>
2022-01-31 22:16:07 +0900

    create link for sign in options

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchIcon from "@material-ui/icons/Search";
+import { Link } from "react-router-dom";
 import "../assets/stylesheets/Header.css";
 
 function Header() {
@@ -14,9 +15,11 @@ function Header() {
         <input type="text" className="header__searchInput" />
         <SearchIcon className="header__searchIcon" />
       </div>
-      <div className="header__option"></div>
-      <span className="header__optionLineOne">Hello User</span>
-      <span className="header__optionLineTwo">Sign In or Sign Out</span>
+      <Link to="/" className="header__link">
+        <div className="header__option"></div>
+        <span className="header__optionLineOne">Hello User</span>
+        <span className="header__optionLineTwo">Sign In or Sign Out</span>
+      </Link>
     </nav>
   );
 }
