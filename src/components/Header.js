95ee0a9e05f0bc42dc1513a9b77fc74f3ca88c1d
OpeDada <opedada@gmail.com>
2022-01-31 22:40:15 +0900

    add cart link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
 import "../assets/stylesheets/Header.css";
 
@@ -38,6 +39,11 @@ function Header() {
           </div>
         </Link>
       </div>
+      <Link to="/checkout" className="header__link">
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+        </div>
+      </Link>
     </nav>
   );
 }
