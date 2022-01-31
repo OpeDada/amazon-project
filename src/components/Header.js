ddf1614f77266e74f404fe6ca86517f094010b1f
OpeDada <opedada@gmail.com>
2022-01-31 16:24:54 +0900

    create header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,22 @@
 import React from "react";
-
+import SearchIcon from "@material-ui/icons/Search";
 function Header() {
-  return <nav className="header"></nav>;
+  return (
+    <nav className="header">
+      <img
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt="logo"
+      />
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      <div className="header__option"></div>
+      <span className="header__optionLineOne">Hello User</span>
+      <span className="header__optionLineTwo">Sign In or Sign Out</span>
+    </nav>
+  );
 }
 
 export default Header;
