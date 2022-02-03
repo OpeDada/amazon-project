6d6b8fea7363f175067f6a0e9a97055339eb2b6d
OpeDada <opedada@gmail.com>
2022-02-03 22:14:55 +0900

    add T and c statement

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,21 @@
 import React from "react";
+import "../assets/stylesheets/Login.css"
 
 function Login() {
   return (
     <div className="login">
       <div className="login__container">
         <h1>Sign In</h1>
-        <form>
+        <form autoComplete="off">
           <h5>Email</h5>
-          <input type="email" />
+          <input type="email" placeholder="Email" />
+          <h5>Password</h5>
+          <input type="password" placeholder="Password" />
+          <button type="submit" className="login__signInButton">
+            Sign In
+          </button>
         </form>
+        <p>By signing in, you agree to Amazon's Terms and Conditions</p>
       </div>
     </div>
   );
