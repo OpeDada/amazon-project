711ae43129bbe77371fe3f0a762665d25c9d5d92
OpeDada <opedada@gmail.com>
2022-02-05 14:39:28 +0900

    populate product function

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../assets/stylesheets/Product.css";
 
-function Product({id, title,image,price,rating}) {
+function Product({ id, title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
@@ -10,7 +10,18 @@ function Product({id, title,image,price,rating}) {
           <small>¥</small>
           <strong>{price}</strong>
         </p>
+        <div className="product__rating">
+          {
+            Array(rating) // makes an array with empty values, 4 in this case [empty, empty. empty, empty]
+              .fill() // fills up the empty array with undefined because in JS u cant map an empty vale. Hence, [undefined, undefined, undefined, undefined]
+              .map((_) => (
+                <p>*</p>
+              )) // .map loops over the array and replaces the undefined with the star. It will be compiled to an array of HTML paragraph elements with a ⭐ as its text, each.
+          }
+        </div>
       </div>
+      <img src={image} alt="" />
+      <button>Add to basket</button>
     </div>
   );
 }
