e8d7e96a5ce8b16c611dc9c99e312adc94b2c363
OpeDada <opedada@gmail.com>
2022-02-05 14:06:08 +0900

    import product component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,25 @@
 import React from "react";
+import Product from "./Product"
 
 function Home() {
-  return <div></div>;
+  return (
+    <div className="home">
+      <img
+        className="home__image"
+        src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.JPG"
+        alt=""
+      />
+      <div className="home__row">
+        <Product
+          id="1"
+          title="Nano Toothbrush"
+          price={800.96}
+          rating={4}
+          image="https://m.media-amazon.com/images/I/51CZok8eeaL._AC_SL1000_.jpg"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
