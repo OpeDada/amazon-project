6f71d6dec8353d5a03237bd436f6076dead9de86
OpeDada <opedada@gmail.com>
2022-02-05 11:10:48 +0900

    add a route to link component

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,10 +5,11 @@ import "../assets/stylesheets/Login.css";
 function Login() {
   return (
     <div className="login">
-      <Link>
+      <Link to="/">
         <img
-          src="assets/images/amazon-logo.png"
-          alt="Amazon logo"
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt=""
         />
       </Link>
       <div className="login__container">
