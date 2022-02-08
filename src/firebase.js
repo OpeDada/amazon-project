7767daa35c65fadf4311159bcfb66c7523c9f318
OpeDada <opedada@gmail.com>
2022-02-08 22:37:44 +0900

    create firebase file

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase"
+
+const firebaseApp = firebase.initializeApp({
+
+  apiKey: "AIzaSyCJYSbFAR7Oja1BkZfO-Bah5Vwph9m9LRs",
+  authDomain: "project-f2a74.firebaseapp.com",
+  projectId: "project-f2a74",
+  storageBucket: "project-f2a74.appspot.com",
+  messagingSenderId: "888779761349",
+  appId: "1:888779761349:web:3611e39f9fb40b44d432ac",
+  measurementId: "G-58PPK739HF"
+
+})
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth()
+
+export {db, auth}
