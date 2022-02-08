f30dfe8e144cd9e2d1ad706ae14e111e6d864251
OpeDada <opedada@gmail.com>
2022-02-08 22:21:53 +0900

    create and import subtotal file

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,10 +1,20 @@
 import React from "react"
+import Subtotal from "./Subtotal"
 import "../assets/stylesheets/Checkout.css";
-
 
 function Checkout() {
   return (
-    <div></div>
+    <div className="checkout">
+      <div className="checkout__left">
+        <div>
+          <h2 className="checkout_title">Your shopping cart is empty</h2>
+          <p>You have no items in your basket. Buy one.</p>
+        </div>
+      </div>
+      <div className="checkout__right">
+          <Subtotal />
+      </div>
+    </div>
   )
 }
 
