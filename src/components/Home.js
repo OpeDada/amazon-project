4b825ad7ffe5b1613ea72481d469df2246fe6315
OpeDada <opedada@gmail.com>
2022-02-08 21:46:24 +0900

    add more products

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,23 +15,44 @@ function Home() {
         <Product
           id="1"
           title="Nano Toothbrush"
-          price={800.96}
+          price={800}
           rating={4}
           image="https://m.media-amazon.com/images/I/51CZok8eeaL._AC_SL1000_.jpg"
         />
         <Product
           id="2"
-          title="Nano Toothbrush"
-          price={800.96}
+          title="SNIDEL x Victorian Mask"
+          price={770}
           rating={4}
-          image="https://m.media-amazon.com/images/I/51CZok8eeaL._AC_SL1000_.jpg"
+          image="https://m.media-amazon.com/images/I/4108iPOB3aL._AC_SL1000_.jpg"
         />
         <Product
           id="3"
-          title="Nano Toothbrush"
-          price={800.96}
+          title="SWIFTBIRD Slip-on Sneakers"
+          price={1500}
           rating={4}
-          image="https://m.media-amazon.com/images/I/51CZok8eeaL._AC_SL1000_.jpg"
+          image="https://m.media-amazon.com/images/I/61A95jgYtYL._AC_UL1001_.jpg"
+        />
+        <Product
+          id="4"
+          title="NONIO(ノニオ) Plus Whitening Dental Rinse Set"
+          price={1500}
+          rating={4}
+          image="https://m.media-amazon.com/images/I/61WEouzfEaL._AC_SL1500_.jpg"
+        />
+        <Product
+          id="5"
+          title="FORRICA Women's Mini Shoulder Bag"
+          price={2977}
+          rating={4}
+          image="https://m.media-amazon.com/images/I/61Ww0Le8YfL._AC_UL1500_.jpg"
+        />
+        <Product
+          id="6"
+          title="https://m.media-amazon.com/images/I/51c8bpkEkGL._AC_SL1060_.jpg"
+          price={1980}
+          rating={4}
+          image="https://m.media-amazon.com/images/I/61A95jgYtYL._AC_UL1001_.jpg"
         />
       </div>
     </div>
