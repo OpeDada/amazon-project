0d152601839c6b8d3fcb04a97bee367d74edabcb
OpeDada <opedada@gmail.com>
2022-02-10 23:14:37 +0900

    add to basket fnc

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,19 @@ import React from "react";
 import "../assets/stylesheets/Product.css";
 
 function Product({ id, title, image, price, rating }) {
+
+  const addToBasket = () => {
+    dispatchEvent({
+    type: "ADD_TO_BASKET",
+    item: {
+      id: id,
+      title: title,
+      image: image,
+      price: price,
+      rating: rating
+    }
+  })
+  }
   return (
     <div className="product">
       <div className="product__info">
@@ -21,7 +34,7 @@ function Product({ id, title, image, price, rating }) {
         </div>
       </div>
       <img src={image} alt="" />
-      <button>Add to basket</button>
+      <button onClick={addToBasket}>Add to basket</button>
     </div>
   );
 }
