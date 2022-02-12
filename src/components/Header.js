2e3e5767555eaa389ea5802196b4fee0aff08c20
OpeDada <opedada@gmail.com>
2022-02-12 22:53:52 +0900

    delete prime section

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,13 +31,6 @@ function Header() {
             <span className="header__optionLineTwo">& orders</span>
           </div>
         </Link>
-        {/* third link */}
-        <Link to="/" className="header__link">
-          <div className="header__option">
-            <span className="header__optionLineOne">Your</span>
-            <span className="header__optionLineTwo">Prime</span>
-          </div>
-        </Link>
       </div>
       {/*basket icon with number */}
       <Link to="/checkout" className="header__link">
