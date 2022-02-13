243f76df5abf5ec9212817882da68a84c86fbb15
OpeDada <opedada@gmail.com>
2022-02-13 23:43:16 +0900

    refactor

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,24 +1,22 @@
 import React from "react";
-import { useStateValue } from '../StateProvider'
+import { useStateValue } from "../StateProvider";
 import "../assets/stylesheets/Product.css";
 
 function Product({ id, title, image, price, rating }) {
-  const [{basket}, dispatch] = useStateValue()
-
-  console.log('basket content', basket)
+  const [{ basket }, dispatch] = useStateValue();
 
   const addToBasket = () => {
     dispatch({
-    type: "ADD_TO_BASKET",
-    item: {
-      id: id,
-      title: title,
-      image: image,
-      price: price,
-      rating: rating
-    }
-  })
-  }
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
   return (
     <div className="product">
       <div className="product__info">
@@ -27,7 +25,7 @@ function Product({ id, title, image, price, rating }) {
           <small>¥</small>
           <strong>{price}</strong>
         </p>
-        <div className="product__rating">
+        <div className="product__rating" key={id}>
           {
             Array(rating) // makes an array with empty values, 4 in this case [empty, empty. empty, empty]
               .fill() // fills up the empty array with undefined because in JS u cant map an empty vale. Hence, [undefined, undefined, undefined, undefined]
