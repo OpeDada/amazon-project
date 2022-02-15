3b6ef9abdd12b26990c77014aaa23e6ce60565df
OpeDada <opedada@gmail.com>
2022-02-15 23:11:47 +0900

    change imports to compat version

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,18 @@
-import firebase from "firebase"
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const firebaseApp = firebase.initializeApp({
-
   apiKey: "AIzaSyCJYSbFAR7Oja1BkZfO-Bah5Vwph9m9LRs",
   authDomain: "project-f2a74.firebaseapp.com",
   projectId: "project-f2a74",
   storageBucket: "project-f2a74.appspot.com",
   messagingSenderId: "888779761349",
   appId: "1:888779761349:web:3611e39f9fb40b44d432ac",
-  measurementId: "G-58PPK739HF"
-
-})
+  measurementId: "G-58PPK739HF",
+});
 
 const db = firebaseApp.firestore();
-const auth = firebase.auth()
+const auth = firebase.auth();
 
-export {db, auth}
+export { db, auth };
