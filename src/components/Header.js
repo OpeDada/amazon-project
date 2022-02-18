12eb6f7ed650001af4bf2ea04fc13e2783de1bda
OpeDada <opedada@gmail.com>
2022-02-18 20:08:56 +0900

    add conditional statements for user

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,17 @@ import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
 import { useStateValue } from "../StateProvider";
 import "../assets/stylesheets/Header.css";
+import { auth } from "../firebase";
 
 function Header() {
 
-  const [{basket}, dispatch] = useStateValue()
+  const [{basket, user}, dispatch] = useStateValue()
+
+  const logoutUser = () => {
+    if(user){
+      auth.signOut()
+    }
+  }
 
   return (
     <nav className="header">
@@ -24,10 +31,10 @@ function Header() {
       </div>
       <div className="header__nav">
         {/* first link */}
-        <Link to="#" className="header__link">
-          <div className="header__option">
-            <span className="header__optionLineOne">Hello, User</span>
-            <span className="header__optionLineTwo">Sign</span>
+        <Link to={!user && "/login"} className="header__link">
+          <div onClick={logoutUser} className="header__option">
+            <span className="header__optionLineOne">Hello, {user?.email}</span>
+            <span className="header__optionLineTwo">{user ? 'Sign Out' : 'Sign In'}</span>
           </div>
         </Link>
         {/* second link */}
