67575becb43030d7e9bec4dec06dae7fea194c20
OpeDada <opedada@gmail.com>
2022-02-18 18:41:36 +0900

    add props and remove item fnc

--- src/components/ProductCart.js
+++ src/components/ProductCart.js
@@ -1,26 +1,23 @@
-import React from "react"
+import React from "react";
 import { useStateValue } from "../StateProvider";
 import "../assets/stylesheets/ProductCart.css";
 
-
-function ProductCart({id, title, image, price, rating}){
-
-  const [{basket}, dispatch] = useStateValue()
+function ProductCart({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
 
   const removeItem = () => {
     dispatch({
-      type: 'REMOVE_FROM_CART',
-      id: id
-    })
-  }
+      type: "REMOVE_FROM_CART",
+      id: id,
+    });
+  };
   return (
     <div className="productcart">
       <img className="productcart__image" src={image} alt="" />
       <div className="productcart__info">
-        <p>{title}</p>
-        <p>¥{price}</p>
-      </div>
-      <div>
+        <p className="productcart__title">{title}</p>
+        <p className="productcart__price">¥{price}</p>
+      <div className="product__rating" key={id}>
         {Array(rating)
           .fill()
           .map((_) => {
@@ -28,8 +25,9 @@ function ProductCart({id, title, image, price, rating}){
           })}
       </div>
       <button onClick={removeItem}>Remove from the Cart</button>
+          </div>
     </div>
   );
 }
 
-export default ProductCart
+export default ProductCart;
