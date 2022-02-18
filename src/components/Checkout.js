8211a418ec6bbc8a4d8fb9c0f975ebe9d410a7aa
OpeDada <opedada@gmail.com>
2022-02-18 16:08:41 +0900

    display products in cart

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,26 +1,42 @@
-import React from "react"
-import Subtotal from "./Subtotal"
+import React from "react";
+import Subtotal from "./Subtotal";
+import ProductCart from "./ProductCart";
 import { useStateValue } from "../StateProvider";
 import "../assets/stylesheets/Checkout.css";
+import Product from "./Product";
 
 function Checkout() {
+  const [{ basket }] = useStateValue();
 
-  const[{basket}] = useStateValue()
-  
   return (
     <div className="checkout">
       <div className="checkout__left">
-        <div>
-          <h2 className="checkout_title">Your shopping cart is empty</h2>
-          <p>You have no items in your basket. Buy one.</p>
-        </div>
+        {basket.length === 0 ? (
+          <div>
+            <h2 className="checkout_title">Your shopping cart is empty</h2>
+            <p>You have no items in your basket. Buy one.</p>
+          </div>
+        ) : (
+          <div>
+            <h2 className="shoppingbaskettitle">Items in the Shopping Cart</h2>
+            {basket.map((item) => (
+              <ProductCart
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+
+              />
+            ))}
+          </div>
+        )}
       </div>
       <div className="checkout__right">
-          <Subtotal />
+        <Subtotal />
       </div>
     </div>
-  )
+  );
 }
 
-
-export default Checkout
+export default Checkout;
